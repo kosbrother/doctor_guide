cfc1c83ea711ab3a9bb60db919fcb0c2cd8215ab
Steven Ko <chunyuko85@gmail.com>
2016-01-06 15:44:06 +0800

    doctor api add hospital name

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::DoctorsController < Api::ApiController
 
     divs = Division.joins(:div_hosp_doc_ships).where("divisions.category_id = #{category_id}").select('doctor_id').uniq{|x| x.doctor_id}
     doc_ids = divs.map{|item| item["doctor_id"].to_i}.join(",")
-    doctors = Doctor.where("id in (#{doc_ids}) and area_id = #{area_id}").select('id,name,address')
+    doctors = Doctor.joins(:hospitals).where("doctors.id in (#{doc_ids}) and doctors.area_id = #{area_id}").select('doctors.id,doctors.name,hospitals.name as hospital')
 
     render :json => doctors
   end
