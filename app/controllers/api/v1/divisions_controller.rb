4dc0f9172c930d7ff6ba90bebb1e96e8368cfbf9
Steven Ko <chunyuko85@gmail.com>
2016-01-07 17:07:50 +0800

    division controller

--- app/controllers/api/v1/divisions_controller.rb
+++ app/controllers/api/v1/divisions_controller.rb
@@ -15,6 +15,9 @@ class Api::V1::DivisionsController < Api::ApiController
   def by_hospital
     hospital_id = params[:hospital_id]
     divisions = Division.joins(:div_hosp_doc_ships).where("div_hosp_doc_ships.hospital_id = #{hospital_id} and div_hosp_doc_ships.doctor_id is not null").uniq{|x| x.id}.select("divisions.id,divisions.name")
+    if divisions.size == 0
+      divisions = Division.joins(:div_hosp_doc_ships).where("div_hosp_doc_ships.hospital_id = #{hospital_id}").uniq{|x| x.id}.select("divisions.id,divisions.name")
+    end
     render :json => divisions
   end
 end
