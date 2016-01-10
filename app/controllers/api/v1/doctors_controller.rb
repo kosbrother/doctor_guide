ee829e7589a111f96f49567308a6cd6caab7f9b9
Steven Ko <chunyuko85@gmail.com>
2016-01-10 18:23:06 +0800

    doctors by_area_category paging

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::DoctorsController < Api::ApiController
 
     divs = Division.joins(:div_hosp_doc_ships).where("divisions.category_id = #{category_id}").select('doctor_id').uniq{|x| x.doctor_id}
     doc_ids = divs.map{|item| item["doctor_id"].to_i}.join(",")
-    doctors = Doctor.joins(:hospitals).where("doctors.id in (#{doc_ids}) and doctors.area_id = #{area_id}").select('doctors.id,doctors.name,hospitals.name as hospital').uniq{|x| x.doctor_id}
+    doctors = Doctor.joins(:hospitals).where("doctors.id in (#{doc_ids}) and doctors.area_id = #{area_id}").select('doctors.id,doctors.name,hospitals.name as hospital').uniq{|x| x.doctor_id}.paginate(:page => params[:page], :per_page => 5)
 
     render :json => doctors
   end
