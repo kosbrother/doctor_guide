d8bc7cfad7c0baa9e1f61aff7ba9f9257d8494c9
varbark <naoki3355@gmail.com>
2016-03-08 20:22:34 +0800

    View - Search by area

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,8 +2,8 @@ class IndexController < ApplicationController
   def index
     @areas = Area.all
     @categories = Category.all
-    @hospitals = Hospital.all
-    @doctors = Doctor.all
+    @goodHospitals = Hospital.all.sort_by { |d| -d.recommend_num}.first(10)
+    @goodDoctors = Doctor.all.sort_by { |d| -d.recommend_num}.first(10)
     @comments = Comment.where.not(div_comment: (nil || "")).paginate(:page => params[:page])
   end
 end
