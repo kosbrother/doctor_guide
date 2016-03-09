ecb102f56bb132de2ea269e9ce51e16ef9c986b0
varbark <naoki3355@gmail.com>
2016-03-09 13:19:08 +0800

    View - information of hospital: layout finish

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,5 +1,10 @@
 class InformationController < ApplicationController
   def hospital
+    @categories = Category.all
+    @hospitals = Hospital.where(area_id: 4).paginate(:page => params[:page])
+    @goodDoctors = Doctor.all.sort_by { |d| -d.recommend_num}.first(10)
+    @goodCategories =  Category.all.first(10)
+    @comments = Comment.where.not(div_comment: (nil || "")).paginate(:page => params[:page])
   end
 
   def category
