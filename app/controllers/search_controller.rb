78e4788abb3eccff5f05cfa9648602865ffd29ab
varbark <naoki3355@gmail.com>
2016-03-10 16:03:54 +0800

    View search by Area & by category: layout finish

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,6 +16,10 @@ class SearchController < ApplicationController
   end
 
   def byAreaCategory
-
+    @categories = Category.all
+    @hospitals = Hospital.where(area_id: 4).paginate(:page => params[:page])
+    @goodDoctors = Doctor.all.sort_by { |d| -d.recommend_num}.first(10)
+    @goodHospitals = Hospital.all.sort_by { |d| -d.recommend_num}.first(10)
+    @comments = Comment.where.not(div_comment: (nil || "")).paginate(:page => params[:page])
   end
 end
