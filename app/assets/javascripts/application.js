000e33537590774d8518b41a5f34ff562b70a89e
varbark <naoki3355@gmail.com>
2016-03-14 21:21:46 +0800

    Fix layout: Recomment hospitals and doctors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var make_button_active = function()
+{
+    //Get item siblings
+    var siblings =($(this).siblings());
+
+    //Remove active class on all buttons
+    siblings.each(function (index)
+        {
+            $(this).removeClass('active');
+        }
+    );
+
+
+    //Add the clicked button class
+    $(this).addClass('active');
+};
+
+//Attach events to menu
+$(document).ready(
+    function()
+    {
+        $(".recommend-lists-tag").click(make_button_active);
+    }
+);
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,9 @@ class IndexController < ApplicationController
     @areas = Area.all
     @categories = Category.all
     @goodHospitals = Hospital.order(avg: :desc).limit(10)
+    @popHospitals = Hospital.order(avg: :desc).limit(10).offset(10)
     @goodDoctors = Doctor.order(avg: :desc).limit(10)
+    @popHospitals = Doctor.order(avg: :desc).limit(10).offset(10)
     @doctorComments = Comment.where.not(dr_comment: nil).where.not(dr_comment: "").paginate(:page => params[:page])
     @divisionComments = Comment.where.not(div_comment: nil).where.not(div_comment: "").paginate(:page => params[:page])
   end
