4d46c78c9b2d1e8aa318d9f4729256d8ea1a8afe
varbark <naoki3355@gmail.com>
2016-03-15 22:58:46 +0800

    Refactor index code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ActiveRecord::Base
   after_destroy :update_score
 
   belongs_to :doctor
-  belongs_to :user
   belongs_to :hospital
   belongs_to :division
   belongs_to :commentor, :class_name => 'User', :foreign_key => 'user_id'
