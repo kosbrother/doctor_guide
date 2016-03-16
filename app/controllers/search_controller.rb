48f690a80e88173409d541e812280e5dd390c728
varbark <naoki3355@gmail.com>
2016-03-16 16:50:16 +0800

    View: recommend hospital and doctor( refactor scss)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,10 @@ class SearchController < ApplicationController
     area_id = params['area']
     @area = Area.find(area_id)
     @areaHospitals = Hospital.where(area_id: area_id).paginate(:page => params[:page]).per_page(10)
+    @areaGoodHospitals = Hospital.where(area_id: area_id).order(avg: :desc).limit(10)
+    @areaPopHospitals = Hospital.where(area_id: area_id).order(avg: :desc).limit(10).offset(10)
+    @areaGoodDoctors = Doctor.where(area_id: area_id).order(avg: :desc).limit(10)
+    @areaPopDoctors = Doctor.where(area_id: area_id).order(avg: :desc).limit(10).offset(10)
   end
 
   def byCategory
