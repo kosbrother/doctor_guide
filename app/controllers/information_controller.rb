7997e0946b7f1f7ec0369107b1bb96a68700cd66
varbark <naoki3355@gmail.com>
2016-03-18 18:29:18 +0800

    View: information box

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -2,14 +2,6 @@ class InformationController < ApplicationController
   def hospital
     hospital_id = params['hospital']
     @hospital = Hospital.find(hospital_id)
-    @area = Area.find(@hospital.area)
-    @areas = Area.all
-    @categories = Category.all
-    @categories = Category.all
-    @hospitals = Hospital.where(area_id: 4).paginate(:page => params[:page])
-    @goodDoctors = Doctor.all.sort_by { |d| -d.recommend_num}.first(10)
-    @goodCategories =  Category.all.first(10)
-    @comments = Comment.where.not(div_comment: (nil || "")).paginate(:page => params[:page])
   end
 
   def category
