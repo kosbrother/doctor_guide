c3dbe6f7ce821fcb7df7da9da39bd94f89052fea
varbark <naoki3355@gmail.com>
2016-03-19 23:26:16 +0800

    Recommend doctor

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -5,6 +5,8 @@ class InformationController < ApplicationController
     @divisions = @hospital.divisions.paginate(:page => params[:page]).uniq{|x| x.name}.per_page(12)
     @goodDivisions =  @hospital.comments.select("name, category_id, sum(div_friendly) as total").joins('LEFT OUTER JOIN `divisions` ON `divisions`.`id` = `comments`.`division_id`').group('division_id').order('total desc').limit(10)
     @popDivisions =  @hospital.comments.select("name, category_id, count(div_comment) as total").joins('LEFT OUTER JOIN `divisions` ON `divisions`.`id` = `comments`.`division_id`').group('division_id').order('total desc').limit(10)
+    @goodDoctors =  @hospital.comments.select("name, sum(dr_friendly) as total").joins('LEFT OUTER JOIN `doctors` ON `doctors`.`id` = `comments`.`doctor_id`').group('doctor_id').order('total desc').limit(10)
+    @popDoctors =  @hospital.comments.select("name, count(dr_comment) as total").joins('LEFT OUTER JOIN `doctors` ON `doctors`.`id` = `comments`.`doctor_id`').group('doctor_id').order('total desc').limit(10)
   end
 
   def category
