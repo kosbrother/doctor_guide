312d596bdea577be11c7ae5b8c87ef0db7f5a5cd
varbark <naoki3355@gmail.com>
2016-03-21 12:27:12 +0800

    Divisions List change to carousel

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -2,7 +2,7 @@ class InformationController < ApplicationController
   def hospital
     hospital_id = params['hospital']
     @hospital = Hospital.find(hospital_id)
-    @divisions = @hospital.divisions.paginate(:page => params[:page]).uniq{|x| x.name}.per_page(12)
+    @divisions = @hospital.divisions.uniq{|x| x.id}
     @goodDivisions =  @hospital.comments.select("name, category_id, sum(div_friendly) as total").joins('LEFT OUTER JOIN `divisions` ON `divisions`.`id` = `comments`.`division_id`').group('division_id').order('total desc').limit(10)
     @popDivisions =  @hospital.comments.select("name, category_id, count(div_comment) as total").joins('LEFT OUTER JOIN `divisions` ON `divisions`.`id` = `comments`.`division_id`').group('division_id').order('total desc').limit(10)
     @goodDoctors =  @hospital.comments.select("name, doctor_id,  sum(dr_friendly) as total").joins('LEFT OUTER JOIN `doctors` ON `doctors`.`id` = `comments`.`doctor_id`').group('doctor_id').order('total desc').limit(10)
