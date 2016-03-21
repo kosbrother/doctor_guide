9919f4c6ea1085d3916aef360dc16f2f7e13a04e
varbark <naoki3355@gmail.com>
2016-03-21 17:34:32 +0800

    Rating box

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -16,7 +16,18 @@ class InformationController < ApplicationController
     @division = @hospital.divisions.find(params['division'])
     #  TODO validate if hospital has this division
     @otherDivisions = @hospital.divisions.where.not(id: params['division'])
-    @comments = @division.comments.where.not(div_comment: (nil || "")).paginate(:page => params[:page]).per_page(3)
+    @comments = Comment.where(hospital_id: params['hospital'], division_id: params['division'])
+    @recommendNum = @comments.where(is_recommend: true).count
+    @commentNum = @comments.where.not(div_comment: (nil || "")).count
+    @divEquipment =  @comments.average(:div_equipment).to_f
+    @divEnvironment = @comments.average(:div_environment).to_f
+    @divSpeciality = @comments.average(:div_speciality).to_f
+    @divFriendly = @comments.average(:div_friendly).to_f
+    @avgRate = ( @divEquipment + @divEnvironment + @divSpeciality + @divFriendly) / 4
+    @drFriendly = @comments.average(:dr_friendly).to_f
+    @drSpeciality = @comments.average(:dr_speciality).to_f
+    @avgDocRate = (@drFriendly + @drSpeciality) / 2
+    @commentsPage =  @comments.where.not(div_comment: (nil || "")).paginate(:page => params[:page]).per_page(3)
   end
 
   def doctor
