2d5df56d7031adc80c552a986e0fdbb17512b0a6
varbark <naoki3355@gmail.com>
2016-03-24 21:09:43 +0800

    add breadCrumb

--- db/schema.rb
+++ db/schema.rb
@@ -111,9 +111,9 @@ ActiveRecord::Schema.define(version: 20160324034050) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "subject",    limit: 255
-    t.string   "content",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
