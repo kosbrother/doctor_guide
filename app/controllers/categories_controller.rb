b7c146aaca79d46ca5aa8b46c6fbafc2367da48f
varbark <naoki3355@gmail.com>
2016-03-24 13:03:38 +0800

    Recommend doctors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,12 @@ class CategoriesController < ApplicationController
       @area = Area.find(params['area_id'])
     end
     @category = Category.find(params['id'])
-    @areaCateHospitals = @category.hospitals.where(area_id: params['area_id']).paginate(:page => params[:page]).per_page(10)
+    @areaCateHospitals = @category.hospitals.where(area_id: params['area_id']).uniq{|x| x.id}.paginate(:page => params[:page]).per_page(10)
+    @areaCateGoodHospitals =  @areaCateHospitals.order('recommend_num desc').limit(10)
+    @areaCatePopHospitals = @areaCateHospitals.order('comment_num desc').limit(10)
+    @areaCateDoctors =  Doctor.joins(:divisions, :div_hosp_doc_ships).where('divisions.category_id = 4 AND doctors.area_id = 4').uniq{|x| x.id}
+    @areaCateGoodDoctors = @areaCateDoctors.order('recommend_num desc').limit(10)
+    @areaCatePopDoctors = @areaCateDoctors.order('comment_num desc').limit(10)
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   get '/areas/:id/hospitals/recommend', to: 'hospitals#area_recommend'
   get '/areas/:id/doctors/recommend', to: 'doctors#area_recommend'
-  get '/areas/:area_id/divisions/:id/doctors/recommend', to: 'doctors#area_division_recommend'
+  get '/areas/:area_id/categories/:id/doctors/recommend', to: 'doctors#area_categories_recommend'
   get '/hospitals/:id/doctors/recommend', to: 'doctors#hospital_recommend'
 
   namespace :admin do
