312d708b45078efa787353fc7d1eb04030c52bdf
varbark <naoki3355@gmail.com>
2016-03-25 17:04:33 +0800

    other comment lists

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,10 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params['id'])
     if params['doctor_id']
       @doctor = Doctor.find(params['doctor_id'])
-    else
+      @comments = Comment.where(doctor_id: @comment.doctor_id).where("id != #{@comment.id}").paginate(:page => params[:page]).per_page(3)
 
+    else
+      @comments = Comment.where(hospital_id: @comment.hospital_id, division_id: @comment.division_id).where("id != #{@comment.id}").paginate(:page => params[:page]).per_page(3)
     end
     @hospital = Hospital.find(params['hospital_id'])
     @division = Division.find(params['division_id'])
