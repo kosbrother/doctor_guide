95c1d5cddc070922ec4a98f58f7a296344962c4d
varbark <naoki3355@gmail.com>
2016-03-30 17:02:25 +0800

    replace with path

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,7 @@ class IndexController < ApplicationController
   def index
     @goodHospitals = Hospital.order(recommend_num: :desc).limit(10)
     @popHospitals = Hospital.order(comment_num: :desc).limit(10)
-    doctors = DivHospDocShip.includes(:hospital, :division, :doctor).where.not('divisions.id' => nil, 'hospital_id' => nil)
+    doctors = DivHospDocShip.includes(:hospital, :division, :doctor).where.not('divisions.id': nil, 'hospital_id': nil)
     @goodDoctors = doctors.order('doctors.recommend_num desc').limit(10)
     @popDoctors = doctors.order('doctors.comment_num desc').limit(10)
     @doctorComments = Comment.includes(:commentor, :doctor, :hospital, :division).where.not(dr_comment: (nil || '')).paginate(:page => params[:page]).per_page(3)
