465b2df9d8e8fe61c72eec8e6c377681f55c7bd5
varbark <naoki3355@gmail.com>
2016-03-30 15:11:13 +0800

    refactor index query

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,11 @@
 class IndexController < ApplicationController
   def index
     @goodHospitals = Hospital.order(recommend_num: :desc).limit(10)
-    @popHospitals = Hospital.order(comment_num: :desc).limit(10).offset(10)
-    @goodDoctors = Doctor.order(recommend_num: :desc).limit(10)
-    @popDoctors = Doctor.order(comment_num: :desc).limit(10).offset(10)
-    @doctorComments = Comment.where.not(dr_comment: nil).where.not(dr_comment: "").paginate(:page => params[:page]).per_page(3)
-    @divisionComments = Comment.where.not(div_comment: nil).where.not(div_comment: "").paginate(:page => params[:page]).per_page(3)
-    @crumb = :root
+    @popHospitals = Hospital.order(comment_num: :desc).limit(10)
+    doctors = DivHospDocShip.includes(:hospital, :division, :doctor).where.not('divisions.id' => nil, 'hospital_id' => nil)
+    @goodDoctors = doctors.order('doctors.recommend_num desc').limit(10)
+    @popDoctors = doctors.order('doctors.comment_num desc').limit(10)
+    @doctorComments = Comment.includes(:commentor, :doctor, :hospital, :division).where.not(dr_comment: (nil || '')).paginate(:page => params[:page]).per_page(3)
+    @divisionComments = Comment.includes(:commentor, :doctor, :hospital, :division).where.not(div_comment: (nil || '')).paginate(:page => params[:page]).per_page(3)
   end
 end
