c66f86822318465a25663c6c77b6947baadc7016
varbark <naoki3355@gmail.com>
2016-04-04 11:10:37 +0800

    add doctor sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,5 +8,11 @@ SitemapGenerator::Sitemap.create do
   Hospital.find_each do |hospital|
     add hospital_path(hospital), :changefreq => 'daily', :priority => 0.9
   end
+  Doctor.find_each do |doctor|
+    hospital = doctor.hospitals[0]
+    doctor.divisions.each do |division|
+      add hospital_division_doctor_path(hospital, division, doctor), :changefreq => 'daily', :priority => 0.9
+    end
+  end
 end
 SitemapGenerator::Sitemap.ping_search_engines
